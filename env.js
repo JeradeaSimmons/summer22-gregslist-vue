945b2f97919c4f3ac5beb76982d96dbe08437c3c
JD Simmons <hippeejd@gmail.com>
2022-08-17 14:36:55 -0600

    n

---
+++ env.js
@@ -0,0 +1,8 @@
+
+
+export const dev = window.location.origin.includes('localhost')
+export const useSockets = false
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'hippeejd.us.auth0.com'
+export const audience = 'https://mycodeworks.com'
+export const clientId = 'sC7bCqbQi9X5OKrtZJ5k9v8NWL6WpLHB'
